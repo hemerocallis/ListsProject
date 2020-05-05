53d0802d1e72d956b0b5f09fc063446acc2e1c84
hemera <hemerocallisongml@gmail.com>
2020-05-05 13:17:35 +0700

    Base view returns minimal HTML

--- todolists/views.py
+++ todolists/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
-def home_page():
+def home_page(request):
     '''main page of the app'''
-    pass
+    return HttpResponse('<html><title>To-Do lists</title></html>')
