687bf0a84e19576149301144b9708e4a18e60dd0
hemera <hemerocallisongml@gmail.com>
2020-05-05 15:28:19 +0700

    functional tests enhancement

--- functional_tests.py
+++ functional_tests.py
@@ -1,5 +1,8 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 import unittest
+import time
 
 class NewVisitorTest(unittest.TestCase):
     '''new user test'''
@@ -14,16 +17,26 @@ class NewVisitorTest(unittest.TestCase):
         # user opens our main page
         self.browser.get('http://localhost:8000')
 
-        # user can see correct page title
-        assert 'To-Do' in self.browser.title,\
-        "Something went wrong :("
+        # user can see correct page title and header
+        self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element(By.TAG_NAME, 'h1').text
+        self.assertIn('To-Do', header_text)
 
         # user can see the offer to enter list item
+        inputbox = self.browser.find_element(By.CSS_SELECTOR, '#id_new_item')
+        self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
 
         # user can enter the first list item
+        inputbox.send_keys('Learn TDD')
 
         # user can save list item: upon the page reload user can see
         # the previously entered list item
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element(By.CSS_SELECTOR, '#id_list_table')
+        rows = table.find_elements(By.TAG_NAME, 'tr')
+        self.assertTrue(any(row.text == '1: Learn TDD' for row in rows))
 
         # user still can see offer to enter list item
 
@@ -32,6 +45,8 @@ class NewVisitorTest(unittest.TestCase):
         # user can see text about unique URL address for the list
 
         # user can open their unique URL address of the created to-do list
+
+        self.fail('End the test!')
 
 if __name__ == '__main__':
     unittest.main()
