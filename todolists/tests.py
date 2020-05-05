e63f1fb6133f571a163225b1e8cdb1874756e535
hemera <hemerocallisongml@gmail.com>
2020-05-05 16:29:30 +0700

    Home page view refactoring (using template)

--- todolists/tests.py
+++ todolists/tests.py
@@ -6,15 +6,7 @@ from todolists.views import home_page
 class HomePageTest(TestCase):
     '''unit tests for main page'''
 
-    def test_root_url_resolves_to_home_page_view(self):
-        '''sample test'''
-        found = resolve('/')
-        self.assertEqual(found.func, home_page)
-
-    def test_home_page_returns_correct_html(self):
-        request = HttpRequest()
-        response = home_page(request)
-        html = response.content.decode('utf8')
-        self.assertTrue(html.startswith('<html>'))
-        self.assertIn('<title>To-Do lists</title>', html)
-        self.assertTrue(html.endswith('</html>'))
+    def test_uses_home_template(self):
+        '''home page template test via Django test client'''
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
