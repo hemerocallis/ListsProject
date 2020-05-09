cbe3e2d6f8a72b1c96d1319b01b36ced6f0d40f2
hemera <hemerocallisongml@gmail.com>
2020-05-09 19:36:11 +0700

    new functional test is added

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,8 +1,12 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import WebDriverException
 from django.test import LiveServerTestCase
+import unittest
 import time
+
+MAX_WAIT = 10
 
 class NewVisitorTest(LiveServerTestCase):
     '''new user test'''
@@ -12,12 +16,21 @@ class NewVisitorTest(LiveServerTestCase):
     def tearDown(self):
         self.browser.quit()
 
-    def check_for_row_in_list_table(self, row_text):
-        table = self.browser.find_element(By.CSS_SELECTOR, '#id_list_table')
-        rows = table.find_elements(By.TAG_NAME, 'tr')
-        self.assertIn(row_text, [row.text for row in rows])
+    def wait_for_row_in_list_table(self, row_text):
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element(By.CSS_SELECTOR, '#id_list_table')
+                rows = table.find_elements(By.TAG_NAME, 'tr')
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
 
-    def test_can_start_a_list_and_retrieve_it_later(self):
+
+    def test_can_start_a_list_for_one_user(self):
         '''user starts to-do list'''
         # user opens our main page
         self.browser.get(self.live_server_url)
@@ -37,9 +50,8 @@ class NewVisitorTest(LiveServerTestCase):
         # user can save list item: upon the page reload user can see
         # the previously entered list item
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
 
-        self.check_for_row_in_list_table('1: Learn TDD')
+        self.wait_for_row_in_list_table('1: Learn TDD')
 
         # user still can see offer to enter list item
         inputbox = self.browser.find_element(By.CSS_SELECTOR, '#id_new_item')
@@ -47,16 +59,54 @@ class NewVisitorTest(LiveServerTestCase):
         # user can create the second list item and see result list
         inputbox.send_keys('Create pet-project')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(3)
 
-        self.check_for_row_in_list_table('1: Learn TDD')
-        self.check_for_row_in_list_table('2: Create pet-project')
+        self.wait_for_row_in_list_table('1: Learn TDD')
+        self.wait_for_row_in_list_table('2: Create pet-project')
 
         # user can see text about unique URL address for the list
 
         # user can open their unique URL address of the created to-do list
 
         self.fail('End the test!')
+
+    def test_multiple_users_can_start_lists_at_different_urls(self):
+        '''multiple users test'''
+        # user1 starts a new list
+        self.browser.get(self.live_server_url)
+        inputbox = self.browser.find_element(By.CSS_SELECTOR, '#id_new_item')
+        inputbox.send_keys('Learn TDD')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Learn TDD')
+
+        # user1 can see unique url of the created list
+        user1_list_url = self.browser.current_url
+        self.assertRegex(user1_list_url, '/lists/.+')
+
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
+
+        # user2 opens home page and don't see user1's list
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element(By.TAG_NAME, 'body').text
+        self.assertNotIn('Learn TDD', page_text)
+        self.assertNotIn('Create pet-project', page_text)
+
+        # user2 starts a new list
+        inputbox = self.browser.find_element(By.CSS_SELECTOR, '#id_new_item')
+        inputbox.send_keys('Automate testing')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Automate testing')
+
+        # user2 takes own unique url
+        user2_list_url = self.browser.current_url
+        self.assertRegex(user2_list_url, '/lists/.+')
+        self.assertNotEqual(user2_list_url, user1_list_url)
+
+        # user2 don't see user1's list
+        page_text = self.browser.find_element(By.TAG_NAME, 'body').text
+        self.assertNotIn('Learn TDD', page_text)
+        self.assertIn('Automate testing', page_text)
+
 
 if __name__ == '__main__':
     unittest.main()
