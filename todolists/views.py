4371383e0aced0fb21d75a911c0d9d34eedfeb32
hemera <hemerocallisongml@gmail.com>
2020-05-10 12:17:09 +0700

    new URL, view and template to display list

--- todolists/views.py
+++ todolists/views.py
@@ -6,7 +6,10 @@ def home_page(request):
     '''main page of the app'''
     if request.method == 'POST':
         Item.objects.create(text=request.POST['item_text'])
-        return redirect('/')
+        return redirect('/lists/unique-list/')
+    return render(request, 'home.html')
 
+def view_list(request):
+    '''to-do list view'''
     items = Item.objects.all()
-    return render(request, 'home.html', {'items': items})
+    return render(request, 'list.html', {'items': items})
