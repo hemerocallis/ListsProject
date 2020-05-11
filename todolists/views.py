f06e0d872220103dfdbd656c74eef2a7829ab8c4
hemera <hemerocallisongml@gmail.com>
2020-05-11 19:42:32 +0700

    new view to add a new item to an existing list

--- todolists/views.py
+++ todolists/views.py
@@ -6,13 +6,19 @@ def home_page(request):
     '''main page of the app'''
     return render(request, 'home.html')
 
-def view_list(request):
+def view_list(request, list_id):
     '''to-do list view'''
-    items = Item.objects.all()
-    return render(request, 'list.html', {'items': items})
+    list_ = List.objects.get(id=list_id)
+    return render(request, 'list.html', {'list': list_})
 
 def new_list(request):
     '''new to-do list view'''
     list_ = List.objects.create()
     Item.objects.create(text=request.POST['item_text'], list=list_)
-    return redirect('/lists/unique-list/')
+    return redirect(f'/lists/{list_.id}/')
+
+def add_item(request, list_id):
+    '''new item view to an existing list'''
+    list_ = List.objects.get(id=list_id)
+    Item.objects.create(text=request.POST['item_text'], list=list_)
+    return redirect(f'/lists/{list_.id}/')
