4299c1659cacd0547de149d6a788978e95139cca
hemera <hemerocallisongml@gmail.com>
2020-05-11 16:14:16 +0700

    new url is added; modified views

--- lists/urls.py
+++ lists/urls.py
@@ -18,5 +18,6 @@ from todolists import views
 
 urlpatterns = [
     url(r'^$', views.home_page, name='home'),
+    url(r'^lists/new$', views.new_list, name='new_list'),
     url(r'^lists/unique-list/$', views.view_list, name='view_list'),
 ]
--- todolists/views.py
+++ todolists/views.py
@@ -4,12 +4,14 @@ from todolists.models import Item
 # Create your views here.
 def home_page(request):
     '''main page of the app'''
-    if request.method == 'POST':
-        Item.objects.create(text=request.POST['item_text'])
-        return redirect('/lists/unique-list/')
     return render(request, 'home.html')
 
 def view_list(request):
     '''to-do list view'''
     items = Item.objects.all()
     return render(request, 'list.html', {'items': items})
+
+def new_list(request):
+    '''new to-do list view'''
+    Item.objects.create(text=request.POST['item_text'])
+    return redirect('/lists/unique-list/')
