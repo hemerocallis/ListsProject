42d83f311ff4b6d9842e932ab7004ed2cb8553f2
hemera <hemerocallisongml@gmail.com>
2020-05-11 17:43:53 +0700

    enhancement of models

--- todolists/models.py
+++ todolists/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class List(models.Model):
+    '''to-do list'''
+    pass
+
 class Item(models.Model):
     '''to-do list item'''
     text = models.TextField(default='')
+    list = models.ForeignKey(List, default=None)
--- todolists/views.py
+++ todolists/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import redirect, render
-from todolists.models import Item
+from todolists.models import Item, List
 
 # Create your views here.
 def home_page(request):
@@ -13,5 +13,6 @@ def view_list(request):
 
 def new_list(request):
     '''new to-do list view'''
-    Item.objects.create(text=request.POST['item_text'])
+    list_ = List.objects.create()
+    Item.objects.create(text=request.POST['item_text'], list=list_)
     return redirect('/lists/unique-list/')
